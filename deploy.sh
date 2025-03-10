3dfe5623bbfbdf27f605b11c25af31aa58b10fed
DuckyBlender <alan@alanklas.com>
2025-03-10 19:48:19 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
+# Stop and remove the old container if it exists
+docker stop dick-grower-bot || true && docker rm dick-grower-bot || true
+
+# Build the new Docker image
 docker build -t dick-bot .
+
+# Run the new container
 docker run -d --restart unless-stopped --name dick-grower-bot -v dick_data:/app/data dick-bot
